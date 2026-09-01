438873e2fe98bf6fd9099165e9267281bae72fec
Shayan <shayanskandari625@gmail.com>
2026-09-01 17:31:15 +0330

    Add show_books function

--- Library_Management_System.py
+++ Library_Management_System.py
@@ -14,8 +14,43 @@ def add_book():
     }
 
     books.append(book)
-    print(books)
 
     print("Book added successfully!")
 
-add_book()
+
+def show_books():
+    if not books:
+        print("No books found!")
+
+    for book in books:
+        print(book["title"])
+        print(book["author"])
+        print(book["year"])
+
+        if book["available"]:
+            print("Available")
+        else:
+            print("Borrowed")
+
+
+def main_menu():
+    while True:
+        print("===== Library Management System =====")
+        print("1. Add Book")
+        print("2. Show Books")
+        print("3. Show Book")
+        print("4. Delete Book")
+        print("5. Borrow Book")
+        print("6. Return Book")
+        print("7. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add_book()
+
+        elif choice == "2":
+            show_books()
+
+
+main_menu()
